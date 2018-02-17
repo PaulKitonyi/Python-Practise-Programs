e4239d40780ca3327841dc3c82f077aebf1dc5c0
Paul Kitonyi <paulmucimah@gmail.com>
2018-02-17 08:30:49 +0300

    A function to return the smallest number in a given list

---
+++ Q19.py
@@ -0,0 +1,14 @@
+'''A function to return the smallest number in a given list'''
+
+def smallest(l):
+    if type(l) != list:
+        raise TypeError('Pass a list')
+    count=0
+    smallest_no=None
+    while count<len(l):
+        if smallest_no==None:
+            smallest_no=l[count]
+        elif l[count]<smallest_no:
+            smallest_no=l[count]
+        count +=1
+    return smallest_no
