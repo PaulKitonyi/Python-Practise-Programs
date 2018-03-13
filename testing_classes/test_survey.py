f62b467c37ecb00895eab9f421c2fa129c7b4e5f
Paul Kitonyi <paulmucimah@gmail.com>
2018-03-13 16:04:32 +0300

    updated scripts

--- testing_classes/test_survey.py
+++ testing_classes/test_survey.py
@@ -4,31 +4,22 @@ from survey import AnonymousSurvey
 class TestAnonymousSurvey(unittest.TestCase):
     """Testing AnonymousSurvey class"""
 
-    # def setUp(self):
-    #     """Instantiate the class"""
-    #     my_survey = AnonymousSurvey("What is your favourite progamming Lang?")
-
-    # def test_show_question(self):
-    #     """Testing the show question method"""
-    #     result = AnonymousSurvey.show_question(my_survey)
-    #     self.assertEqual(result,'What is your favourite progamming Lang?')
+    def setUp(self):
+        """Create a survey and a set of responses for use in all test methods."""
+        question  = 'What is your best editor?'
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['VScode','Atom','Subl']
 
     def test_store_single_response(self):
         """Test that a single response is stored properly."""
-        question  = 'what is your favourite Computer language?'
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('Python')
-
-        self.assertIn('Python',my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_three_responses(self):
-        """Test whether the function can correctly store 3 responses"""
-        question  = 'What is your best editor?'
-        my_survey = AnonymousSurvey(question)
-        responses = ['VScode','Atom','Subl']
-        for response in responses:
-            my_survey.store_response(response)
-        for response in responses:
-            self.assertIn(response,my_survey.responses)
+        """Test that three individual responses are stored properly."""
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.responses:
+            self.assertIn(response,self.my_survey.responses)
 
-# unittest.main()
+unittest.main()
